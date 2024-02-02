12b25e25ec63a00d534dcab5600011d78004a88a
SamHaggans <sam@samhaggans.com>
2024-02-01 19:26:30 -0500

    Add per-flight log files

--- ITAAFlightSoftware.ino
+++ ITAAFlightSoftware.ino
@@ -1,77 +1,125 @@
 #include "hardware_manager.h"
+#include "movingAvg.h"
 
-enum states {
+/**
+  * @brief Calculate altitude difference between a ground pressure and a current pressure.
+  * @param pres current pressure, atmospheres
+  * @param pressureReference reference pressure (i.e. at ground level)
+  * @return Altitude above reference level in meters
+*/
+float pressure_to_meters(float pres, float pressureReference) {
+  double lapseRate = -0.0065;    // valid below 11000m
+  double tempRef = 20 + 273.15;  // C to K
+  double rDryAir = 287.0474909;  // J/K/kg
+  double gAccelEarth = 9.80665;  // m/s**2
+  return (tempRef / lapseRate) * (pow(pres / pressureReference, -rDryAir * lapseRate / gAccelEarth) - 1);
+}
+
+// Creates an instance of the HardwareManager
+
+const int delay_period_ms = 16;
+int last_time_ms = millis();
+
+HardwareManager hardware_manager;
+
+const int launch_accel_threshold = 30;  // meters per second squared
+const int launch_accel_time = 300;      // ms required being over launch_accel_threshold before being sure
+uint32_t accel_threshold_time;
+float current_altitude_meters = 0.0;
+float last_altitude_meters = 0.0;
+float pressure_reference = 1.0;
+movingAvg altitude_avg_filter(30);
+float last_moving_average_altitude = 0;
+float current_moving_average_altitude = 0;
+
+
+enum STATE {
   PREFLIGHT,
   ASCENT,
   DESCENT,
-  POSTFLIGHT
+  POSTFLIGHT,
+  NUM_STATES,
 };
 
 int state;
+int state_count;
 
-// How many times a state has run
-int state_counter = 0;
 
-// Creates an instance of the HardwareManager
-HardwareManager hardware_manager;
-
-// The threshold of acceleration that we want to see
-const int launch_accel_threshold = 30; // m/s^2
-// The time for which we need to continuously see acceleration above the threshold
-const int launch_accel_time = 300; // ms of sustained acceleration
-// The first time (milliseconds) that we have started being above our threshold
-uint32_t accel_threshold_true_since;
-
-// How fast we loop
-const int delay_period_ms = 15;
-int last_time_ms;
 
 void setup() {
-    state = PREFLIGHT;
-    Serial.begin(9600);
-    while (!Serial) {}
-    delay(1000);
-    // Initialize the hardware manager here:
-    hardware_manager.init();
-    accel_threshold_true_since = millis();
+  Serial.begin(9600);
+  while (!Serial) {}
+  delay(3000);
+
+  // Initialize the hardware manager here:
+  hardware_manager.init();
+
+  state = PREFLIGHT;
+  state_count = 0;
+  accel_threshold_time = millis();
+  altitude_avg_filter.begin();
 }
 
 void loop() {
-    if (millis() - last_time_ms < delay_period_ms) {
-      return;
-    }
-    last_time_ms = millis();
-    // Add code that does things with the hardware manager here:
-    // What do we need to do each loop?
-    hardware_manager.readSensorData();
-    hardware_manager.logDataPacket();
-    SensorData_t data = hardware_manager.getSensorData();
-    switch(state) {
-      case PREFLIGHT:
-        //Serial.println("Preflight!");
-        if (!(abs(data.accel_x) > launch_accel_threshold)) {
-          accel_threshold_true_since = millis(); // it has been 0 ms since we started being in the valid acceleration state
-        }
-        if (millis() - accel_threshold_true_since > launch_accel_time) {
-          state = ASCENT;
-        }
-        if (data.pressure < 900) {
-          state = ASCENT;
-        }
-        break;
-      case ASCENT:
-        Serial.println("Ascent!");
-        break;
-      case DESCENT:
-        Serial.println("Descent!");       
-        break;
-      case POSTFLIGHT:
-        Serial.println("Postflight!");
-        break;
-      default:
-        break;
-    }
+  if (millis() - last_time_ms < delay_period_ms) {
+    return;
+  }
+  last_time_ms = millis();
+
+  hardware_manager.readSensorData();
+  hardware_manager.logDataPacket();
+  SensorData_t data = hardware_manager.getSensorData();
+
+  last_altitude_meters = current_altitude_meters;
+  current_altitude_meters = pressure_to_meters(data.pressure, pressure_reference);
+
+  altitude_avg_filter.reading(current_altitude_meters * 1000);
+
+  last_moving_average_altitude = current_moving_average_altitude;
+  current_moving_average_altitude = altitude_avg_filter.getAvg();
 
 
+  // Serial.print("pressure:");
+  // Serial.print(data.pressure);
+  // Serial.print(",");
+  // Serial.print("altitude_meters:");
+  // Serial.print(current_altitude_meters);
+  // Serial.print(",");
+  // Serial.print("accel:");
+  // Serial.println(data.accel_x);
 
+
+  switch (state) {
+    case PREFLIGHT:
+      // update ground level pressure reference
+      pressure_reference = data.pressure;
+
+      //Serial.println("Preflight");
+
+      if (!(data.accel_x > launch_accel_threshold)) {
+        accel_threshold_time = millis();
+      }
+
+      if (millis() - accel_threshold_time > launch_accel_time) {
+        state = ASCENT;
+      }
+
+      break;
+    case ASCENT:
+      Serial.println("Ascent");
+      if (current_moving_average_altitude / 1000.0 < last_moving_average_altitude / 1000.0) {
+        state = DESCENT;
+      }
+      break;
+    case DESCENT:
+      Serial.println("Descent");
+      break;
+    case POSTFLIGHT:
+      // Serial.println("Postflight");
+      break;
+    default:
+      break;
+  }
+
+  state_count++;
 }
--- hardware_manager.cpp
+++ hardware_manager.cpp
@@ -3,9 +3,48 @@
 #include <Wire.h>
 #include <SPI.h>
 
-// Function stubs for everything in hardware_manager.h (just so the project will compile)
-// These will all need to be created to do something!
+File root;
+int currentFlightNum;
 
+/**
+  * @brief Gets a random scalar multiplier to simulate noise
+  * @param noisePercent Percent of noise, i.e. 0.01 = 1% maximum deviation
+  * @return Random noise scalar as a float.
+*/
+float random_scalar(float noisePercent = 0.0005) {
+  return ((float)random(200000 * (1.0-noisePercent), 200000 * (1.0+noisePercent)) / (float)200000); 
+}
+
+/**
+  * @brief Simulated acceleration values
+  * @param t Timestamp in milliseconds
+  * @return simulated acceleration in m/s^2.
+*/
+float test_accelll(uint32_t t) {
+  if (t < 5000) {
+    return 0;
+  } else if (t < 8000) {
+    return 40;
+  } else {
+    return 0;
+  }
+}
+
+/**
+  * @brief Simulated pressure values
+  * @param t Timestamp in milliseconds
+  * @return simulated pressure in atmospheres.
+*/
+float test_pressureeee(uint32_t t) {
+  float scalar = random_scalar();
+  if (t < 5000) {
+    return scalar * 1;
+  } else if (t < 55000) {
+    return scalar * pow((t - 30000.0) / 90000.0, 2) + 0.923;
+  } else {
+    return scalar * 1;
+  }
+}
 
 // Can also use SPI!
 const int SPI_RX = 4;
@@ -16,6 +55,41 @@ const int SD_CS = 17;
 
 const int I2C_SDA = 0;
 const int I2C_SCL = 1;
+
+void printDirectory(File dir, int numTabs) {
+  while (true) {
+    File entry =  dir.openNextFile();
+    if (! entry) {
+      // no more files
+      break;
+    }
+    for (uint8_t i = 0; i < numTabs; i++) {
+      Serial.print('\t');
+    }
+    Serial.print(entry.name());
+    if (!entry.isDirectory()) {
+      // files have sizes, directories do not
+      Serial.print("\t\t");
+      Serial.println(entry.size(), DEC);
+    }
+    entry.close();
+  }
+}
+
+int countExistingLogFiles(File dir) {
+  int count = 0;
+  while (true) {
+    File entry =  dir.openNextFile();
+    if (!entry) {
+      return count;
+    }
+    if (!entry.isDirectory()) {
+      ++count;
+    }
+    entry.close();
+  }
+}
+
 
 void HardwareManager::init() {
     Wire.setSDA(I2C_SDA);
@@ -49,14 +123,21 @@ void HardwareManager::init() {
     } else {
       Serial.println("SD card initialized!");
     }
+    root = SD.open("/");
+    printDirectory(root, 0);
+    currentFlightNum = countExistingLogFiles(root);
+    root.close();
 }
+
+
 
 void HardwareManager::readSensorData() {
   m_sensorData.timestamp = millis();
   sensors_event_t temp_event, pressure_event;
   dps.getEvents(&temp_event, &pressure_event);
   m_sensorData.temperature = temp_event.temperature;
-  m_sensorData.pressure = pressure_event.pressure;
+  m_sensorData.pressure = pressure_event.pressure * 0.000987;
+  // m_sensorData.pressure = test_pressure(millis());
 
   sensors_event_t accel;
   sensors_event_t gyro;
@@ -64,6 +145,7 @@ void HardwareManager::readSensorData() {
   lsm6ds3trc.getEvent(&accel, &gyro, &temp);
 
   m_sensorData.accel_x = accel.acceleration.x;
+  //m_sensorData.accel_x = test_accel(millis());
   m_sensorData.accel_y = accel.acceleration.y;
   m_sensorData.accel_z = accel.acceleration.z;
 
@@ -78,26 +160,26 @@ SensorData_t& HardwareManager::getSensorData() {
 }
 
 void HardwareManager::logDataPacket() {
-  File log_file = SD.open("data_log.txt", FILE_WRITE);
+  File log_file = SD.open((String("flight_log_") + String(currentFlightNum) + String(".txt")).c_str(), FILE_WRITE);
   log_file.print(m_sensorData.timestamp);
   log_file.print(",");
-  log_file.print(m_sensorData.accel_x);
+  log_file.print(m_sensorData.accel_x, 7);
   log_file.print(",");
-  log_file.print(m_sensorData.accel_y);
+  log_file.print(m_sensorData.accel_y, 7);
   log_file.print(",");
-  log_file.print(m_sensorData.accel_z);
+  log_file.print(m_sensorData.accel_z, 7);
   log_file.print(",");
-  log_file.print(m_sensorData.angular_velocity_x);
+  log_file.print(m_sensorData.angular_velocity_x, 7);
   log_file.print(",");
-  log_file.print(m_sensorData.angular_velocity_y);
+  log_file.print(m_sensorData.angular_velocity_y, 7);
   log_file.print(",");
-  log_file.print(m_sensorData.angular_velocity_z);
+  log_file.print(m_sensorData.angular_velocity_z, 7);
   log_file.print(",");
-  log_file.print(m_sensorData.imu_temperature);
+  log_file.print(m_sensorData.imu_temperature, 7);
   log_file.print(",");
-  log_file.print(m_sensorData.pressure);
+  log_file.print(m_sensorData.pressure, 7);
   log_file.print(",");
-  log_file.print(m_sensorData.temperature);
+  log_file.print(m_sensorData.temperature, 7);
   log_file.println();
   log_file.close();
 }
